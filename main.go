d8387626dcc25406c3e83e8bbcbc4a62c9b5f773
Nguyễn Bảo Linh <linh.nguyenbao@ncc.asia>
2025-08-12 13:12:54 +0700

    add handle panic for main

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
 	"mmn/cmd"
+	"runtime/debug"
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("[FATAL] Panic: %v\n%s", r, debug.Stack())
+		}
+	}()
+
 	cmd.Execute()
 }
