1f86146ca5278db5fea1e45fde7c28cd46c9dfc0
duc.tomanh <duc.tomanh@ncc.asia>
2025-09-09 16:55:43 +0700

    remove redundant file

--- mmn-client-js/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-// Keep this package from blocking Nx graph build/lint when local devDeps aren't installed.
-// We ignore all files here and rely on the root ESLint setup for linting.
-module.exports = {
-  root: false,
-  ignorePatterns: ['**/*']
-};
