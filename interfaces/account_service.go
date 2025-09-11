f02c11dfd84bd8baf4902ea60d86b99d8604aa27
duc.tomanh <116334780+ductm-ncc@users.noreply.github.com>
2025-09-11 10:40:21 +0700

    Merge pull request #179 from mezonai/feature/mmn-168
    Feature/mmn 168: Implement json-rpc server

---
+++ interfaces/account_service.go
@@ -0,0 +1,16 @@
+package interfaces
+
+import (
+	"context"
+
+	pb "github.com/mezonai/mmn/proto"
+)
+
+type AccountService interface {
+	GetAccount(ctx context.Context, in *pb.GetAccountRequest) (*pb.GetAccountResponse, error)
+	GetTxHistory(ctx context.Context, in *pb.GetTxHistoryRequest) (*pb.GetTxHistoryResponse, error)
+	GetCurrentNonce(ctx context.Context, in *pb.GetCurrentNonceRequest) (*pb.GetCurrentNonceResponse, error)
+	GetAccountByAddress(ctx context.Context, in *pb.GetAccountByAddressRequest) (*pb.GetAccountByAddressResponse, error)
+}
+
+
