1f094b41f79577fd24ffca6c82e6040d87888360
hoangdohuy <hoang.dohuy@ncc.asia>
2025-10-03 14:26:12 +0700

    mmn-253: logging for ledger

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -174,8 +174,6 @@ func (l *Ledger) ApplyBlock(b *block.Block) error {
 				l.txTracker.RemoveTransaction(txHash)
 			}
 
-			// commit the update
-			logx.Debug("LEDGER", fmt.Sprintf("Applied tx %s => sender: %+v, recipient: %+v\n", tx.Hash(), sender, recipient))
 			if err := l.accountStore.StoreBatch([]*types.Account{sender, recipient}); err != nil {
 				if l.eventRouter != nil {
 					event := events.NewTransactionFailed(tx, fmt.Sprintf("WAL write failed for block %d: %v", b.Slot, err))
@@ -191,9 +189,11 @@ func (l *Ledger) ApplyBlock(b *block.Block) error {
 				}
 				return err
 			}
+			logx.Debug("LEDGER", fmt.Sprintf("Applied tx %s => sender: %+v, recipient: %+v\n", tx.Hash(), sender, recipient))
 		}
 		if len(txMetas) > 0 {
 			l.txMetaStore.StoreBatch(txMetas)
+			logx.Info("LEDGER", fmt.Sprintf("Stored tx metas for block=%d, len=%d", b.Slot, len(txMetas)))
 		}
 	}
 
