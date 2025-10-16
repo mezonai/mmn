12247f67c014ecbcaddfc45f91460fc940dbf84d
Nguyễn Bảo Linh <linh.nguyenbao@ncc.asia>
2025-10-16 15:05:17 +0700

    init staking

---
+++ staking/service.go
@@ -0,0 +1,48 @@
+package staking
+
+import (
+	"sync"
+	"time"
+
+	"github.com/mezonai/mmn/wallet"
+)
+
+
+type Stake struct {
+	wallet 		*wallet.Wallet
+	stakeType 	string
+	stakedAt	time.Time
+}
+
+type StakingPool struct {
+	apr 		uint64
+	fee 		uint64
+	stakers		map[string]*Stake
+	total		uint64
+
+	mu         	sync.RWMutex
+
+}
+
+func NewStake(apr uint64, fee uint64) *StakingPool {
+	return &StakingPool{
+		apr: apr,
+		fee: fee,
+		stakers: make(map[string]*Stake),
+		total: 0,
+	}
+}
+
+
+func (stake *StakingPool) ChangeComission(newApr uint64) {
+	stake.apr = newApr
+}
+
+func (stake *StakingPool) ChangeFee(fee uint64) {
+	stake.fee = fee
+}
+
+
+func Delegate(wallet wallet.Wallet, amount uint64) {
+
+}
