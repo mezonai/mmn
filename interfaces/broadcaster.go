22522e4e902f834712bd49fa0785310e0a44845e
tuananhncc <anh.nguyentuan2@ncc.asia>
2025-10-17 17:38:56 +0700

    343-remove-redudant-grpc

--- interfaces/broadcaster.go
+++ interfaces/broadcaster.go
@@ -3,9 +3,13 @@ package interfaces
 import (
 	"context"
 
+	"github.com/mezonai/mmn/block"
+	"github.com/mezonai/mmn/consensus"
 	"github.com/mezonai/mmn/transaction"
 )
 
 type Broadcaster interface {
+	BroadcastBlock(ctx context.Context, blk *block.BroadcastedBlock) error
+	BroadcastVote(ctx context.Context, vt *consensus.Vote) error
 	TxBroadcast(ctx context.Context, tx *transaction.Transaction) error
 }
