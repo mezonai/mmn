7dae8eacca363432e5b3dce42003075a3bba9534
linh-nguyenbao <linh.nguyenbao@ncc.asia>
2025-10-23 10:39:11 +0700

    Merge pull request #372 from mezonai/mmn/nginx-ip-extra
    mmn nginx ip extract

--- jsonrpc/utils.go
+++ jsonrpc/utils.go
@@ -5,6 +5,8 @@ import (
 	"net"
 	"net/http"
 	"strings"
+
+	"github.com/mezonai/mmn/logx"
 )
 
 // JSON-RPC Method name constants
@@ -33,6 +35,14 @@ func parseJSONRPCRequest(body []byte) *jsonRPCRequest {
 }
 
 func extractClientIPFromRequest(r *http.Request) string {
+	logx.Debug("Extracting client IP from request headers", "X-Real-IP", r.Header.Get("X-Real-IP"), "X-Forwarded-For", r.Header.Get("X-Forwarded-For"), "RemoteAddr", r.RemoteAddr, "CF-Connecting-IP", r.Header.Get("CF-Connecting-IP"))
+	if xri := r.Header.Get("X-Real-IP"); xri != "" {
+		ip := strings.TrimSpace(xri)
+		if net.ParseIP(ip) != nil {
+			return ip
+		}
+	}
+
 	if xff := r.Header.Get("X-Forwarded-For"); xff != "" {
 		parts := strings.Split(xff, ",")
 		if len(parts) > 0 {
